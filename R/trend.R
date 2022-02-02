e2c0ac83e413627377553bb7d391088a7b2ff060
Bartek Kroczek <bartekkroczek@users.noreply.github.com>
2022-02-02 16:22:37 +0100

    Naming convention changed

--- R/trend.R
+++ R/trend.R
@@ -1,18 +1,18 @@
 #' Generic print function for class 'trend'
 #'
 #' Prints trend detected in the signal.
-#' @seealso [gettrend()], [getltrend()], [plot.trend()], [time.trend()], [detrended()], [orisignal()]
+#' @seealso [gettrend()], [getltrend()], [plot_trend()], [time_trend()], [detrended()], [orisignal()]
 #' @export
-print.trend <- function(x,...){
+print_trend <- function(x,...){
   print.default(as.vector(x),...)
 }
 
 #' Generic plot function for class 'trend'
 #'
 #' Plots trend detected in the signal alongside the signal.
-#' @seealso [gettrend()], [getltrend()], [time.trend()], [detrended()], [orisignal()]
+#' @seealso [gettrend()], [getltrend()], [time_trend()], [detrended()], [orisignal()]
 #' @export
-plot.trend <- function(x,...){
+plot_trend <- function(x,...){
   layout(matrix(c(1,2), nrow=2))
   plot.default(x=attr(x, "time"), y=attr(x, "values"), type="l", xlab="Time", ylab="Values + trend",...)
   lines(x=attr(x, "time"), y=x, col="red",...)
@@ -23,14 +23,14 @@ plot.trend <- function(x,...){
 #' Returns the time vector for the trend.
 #' @seealso [gettrend()], [getltrend()], [plot.trend()], [detrended()], [orisignal()]
 #' @export
-time.trend <- function(x,...){
+time_trend <- function(x,...){
   attr(x, "time")
 }
 
 #' Function for class 'trend'
 #'
 #' Returns the detrended signal for the trend.
-#' @seealso [gettrend()], [getltrend()], [plot.trend()], [time.trend()], [orisignal()]
+#' @seealso [gettrend()], [getltrend()], [plot_trend()], [time_trend()], [orisignal()]
 #' @export
 detrended <- function(x,...){
   attr(x, "detrended")
@@ -39,7 +39,7 @@ detrended <- function(x,...){
 #' Function for class 'trend'
 #'
 #' Returns the initial signal for the trend.
-#' @seealso [gettrend()], [getltrend()], [plot.trend()], [time.trend()], [detrended()]
+#' @seealso [gettrend()], [getltrend()], [plot_trend()], [time_trend()], [detrended()]
 #' @export
 orisignal <- function(x,...){
   attr(x, "values")
